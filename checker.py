dfe4c43c0e2d049e4e5495f88f0d33ceada390e4
mz-mukhtar <mahizeki037@gmail.com>
2025-06-15 22:53:06 +0300

    Added base structure and password input

--- checker.py
+++ checker.py
@@ -1,1 +1,17 @@
-print("Welcome to the Password Complexity Checker")
+import getpass
+
+
+def main():
+	print("Welcome to the Password Complexity Checker!\n")
+
+	password = getpass.getpass("Enter your password: ")
+
+
+
+
+
+	print("\n Analyzing password...")
+	print("This is where your strength feedback will appear.")
+
+if __name__ == "__main__":
+	main()
